ff736a16b604f7b0fcb53881965fd4328a524ef5
TaylanTorres09 <taylantorres@gmail.com>
2023-06-15 15:37:55 -0300

    Questão 1 completa

--- peraio.php
+++ peraio.php
@@ -3,13 +3,18 @@
         <title>Calculo Perimetro e raio</title>
     </head>
     <body>
-        <form  action="home.php" method="get">
+
+        <p>
+            Questão: Escreva um script que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo.
+Para saber o valor do pi, use: M_PI (ele armazena o valor de pi).
+        </p>
+
+        <form  action="peraio.php" method="get">
         Numero 1: <input type="text" name="ray"><br>
         <input type="submit">
         </form>
         <?php
-            // $ray = (int)readline('Enter a integer: ');
-            $ray = 2;
+            $ray = $_GET['ray'];
             $pi = M_PI;
 
             $per = 2*$pi*$ray;
