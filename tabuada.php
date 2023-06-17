2358fef4d3e2c8a28a20d07298b2ace4454f92d8
TaylanTorres09 <taylantorres@gmail.com>
2023-06-17 14:35:36 -0300

    Tabuada com as 4 operações

--- tabuada.php
+++ tabuada.php
@@ -11,17 +11,44 @@
 
     <form action="tabuada.php" method="get" id="form">
         Numero: <input type="text" name="num"><br>
-        <input type="submit">
+        
+        <select name="operation">
+            <option value="adi">Adição</option>
+            <option value="sub">Subtração</option>
+            <option value="mult">Multiplicação</option>
+            <option value="div">divisão</option>
+        </select>
+        <input type="submit" , name="calculate" , value="Calcular">
     </form>
     <?php
-    $num = $_GET['num'];
+
+    function tabuada($num, $i, $operation) {
+        switch($operation) {
+            case 'adi':
+                return array('value' => $num + $i, 'op' => '+');
+                break;
+            case 'sub':
+                return array('value' => $num - $i, 'op' => '-');
+                break;
+            case 'mult':
+                return array('value' => $num * $i, 'op' => 'x');
+                break;
+            case 'div':
+                return array('value' => $num / $i, 'op' => '/');
+                break;
+        }
+    }
 
     echo str_repeat('-', 10);
     echo '<br/>';
-    if ($num) {
+    if (@$_GET['calculate']) {
+        $num = $_GET['num'];
+        $operation = $_GET['operation'];
         for ($i = 1; $i <= 10; $i++) {
-            $mult = $num * $i;
-            echo "$num x $i = $mult";
+            $values = tabuada($num, $i, $operation);
+            $value = $values['value'];
+            $op = $values['op'];
+            echo "$num $op $i = $value";
             echo '<br/>';
             echo str_repeat('-', 10);
             echo '<br/>';
