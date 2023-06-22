7af0f359569e8e1d1db969053001221df2733e62
TaylanTorres09 <taylantorres@gmail.com>
2023-06-22 09:59:19 -0300

    encode array to Json

--- leitura_avancada/json/json.php
+++ leitura_avancada/json/json.php
@@ -10,3 +10,9 @@ echo "decode Json to array: ";
 $arr = json_decode($json, true);
 var_dump($arr);
 echo '<br/>';
+
+$arr = ['nome'=>'Ostrogogildo','idade'=>29,'profissao'=>'desenvolvedor'];
+
+echo "encode array to Json: ";
+$json = json_encode($arr);
+echo "$json <br/>";
